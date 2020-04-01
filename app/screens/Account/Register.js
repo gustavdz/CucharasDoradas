b767eecc4efbd83784b1172d418b943bfe34afc7
Gustavo Decker Zambrano <gustavdz@gmail.com>
2020-04-01 11:10:16 -0500

    creacion de formulario registro

--- app/screens/Account/Register.js
+++ app/screens/Account/Register.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { StyleSheet, View, Text} from "react-native";
+import { StyleSheet, View, Image, Text} from "react-native";
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import RegisterForm from "../../components/Account/RegisterForm";
 
 export default function Register() {
     return (
-        <View>
-            <Text>Registro de usuarios</Text>
-        </View>
+        <KeyboardAwareScrollView>
+            <Image
+                source={require('../../../assets/img/original.png')}
+                style={styles.logo}
+                resizeMode='contain'
+            />
+            <View style={styles.viewForm}>
+                <RegisterForm/>
+            </View>
+        </KeyboardAwareScrollView>
     );
 }
+
+const styles = StyleSheet.create({
+    logo: {
+        width: '100%',
+        height: 150,
+        marginTop:20
+    },
+    viewForm: {
+        marginRight: 40,
+        marginLeft: 40,
+
+    }
+});
