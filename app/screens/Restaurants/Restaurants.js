0fc68ee4794d31f81ff8a73a2f4a39efc20148f8
Gustavo Decker Zambrano <gustavdz@gmail.com>
2020-04-11 15:25:44 -0500

    carga de restaurantes

--- app/screens/Restaurants/Restaurants.js
+++ app/screens/Restaurants/Restaurants.js
@@ -1,11 +1,21 @@
 import React, {useState, useEffect} from "react";
 import { StyleSheet, View, Text } from "react-native";
 import ActionButton from "react-native-action-button";
-import * as firebase from "firebase";
+import firebaseApp from "../../utils/FireBase";
+import firebase from "firebase/app";
+import 'firebase/firestore';
+import ListRestaurants from "../../components/Restaurants/ListRestaurants";
+
+const db = firebase.firestore(firebaseApp);
 
 export default function Restaurants(props) {
     const {navigation} = props;
     const [user, setUser] = useState(null);
+    const [restaurants, setRestaurants] = useState([]);
+    const [startRestaurants, setStartRestaurants] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [totalRestaurants, setTotalRestaurants] = useState(0);
+    const limitRestaurants = 8;
 
     useEffect( () => {
         firebase.auth().onAuthStateChanged(userInfo => {
@@ -13,9 +23,28 @@ export default function Restaurants(props) {
         });
     },[]);
 
+    useEffect(() => {
+        db.collection('restaurants').get().then((snap) => {
+            setTotalRestaurants(snap.size);
+            (async() => {
+                const resultRestaurants = [];
+                const restaurants = db.collection('restaurants').orderBy('createdAt', 'desc').limit(limitRestaurants);
+                await restaurants.get().then(response => {
+                    setStartRestaurants(response.docs[response.docs.length - 1]);
+                    response.forEach(doc => {
+                        let restaurant = doc.data();
+                        restaurant.id = doc.id;
+                        resultRestaurants.push({restaurant});
+                    });
+                    setRestaurants(resultRestaurants)
+                });
+            })();
+        });
+    },[]);
+
     return (
         <View style={styles.viewBody}>
-            <Text>Estamos en el restaurante.</Text>
+            <ListRestaurants restaurants={restaurants} isLoading={isLoading} />
 
             {user && <AddRestaurantButton navigation={navigation}/>}
         </View>
